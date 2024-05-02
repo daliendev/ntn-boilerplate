c4f5f91303e0cb638a8c0dd7d84489f489ccef26
dalien <daniel.cadeau13@gmail.com>
2024-05-02 20:35:01 +0000

    [little fixes]

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,13 @@
 <template>
   <main class="mt-2">
     <section class="bg-[#0a0909] grid lg:grid-cols-2">
-      <img src="/images/verbaland/header_picture.JPG" class="xl:max-h-none xl:h-[calc(100svh-56px)] w-svw xl:w-full object-cover" />
-      <div class="flex items-center justify-center m-8">
-        <div class="bg-secondary-950 text-third-100 transition-all rounded-lg">
+      <img src="/images/verbaland/header_picture.JPG"
+      draggable="false"
+      alt="Verbaland держится как царь на берузовом фоне. Красота братцы."
+      class="xl:max-h-none xl:h-[calc(100svh-56px)] w-svw xl:w-full object-cover" />
+      <div v-if="lastRelease"
+      class="flex items-center justify-center m-8">
+      <div class="bg-secondary-800 text-third-100 transition-all rounded-lg">
         <nuxt-link :to="lastRelease._path" class="card card--clickable">
           <img v-if="lastRelease.cover"
           class="cover-image w-full h-72 xl:h-[32rem] object-cover rounded-t-lg" 
@@ -16,22 +20,22 @@
             </h3>
           </span>
         </nuxt-link>
-
+        
         <ul class="mx-8 my-4 xl:mx-16 xl:my-8 flex justify-center flex-wrap gap-2">
-        <li v-for="link in lastRelease.links"
-        :key="link.id">
+          <li v-for="link in lastRelease.links"
+          :key="link.id">
           <a :href="link.url" class="block text-third-100 w-fit border-2 border-third-100 px-4 py-2 rounded-full hover:text-secondary-950 text-xs xl:text-base hover:bg-third-100 transition-all">
             {{ link.label }}
           </a>
         </li>
       </ul>
-      </div>
-      </div>
-    </section>
-    <section class="py-8 bg-secondary-950">
-      <posts :amount="4" />
-    </section>
-  </main>
+    </div>
+  </div>
+</section>
+<section class="py-8 bg-secondary-950">
+  <posts :amount="4" />
+</section>
+</main>
 </template>
 
 <script setup>
@@ -47,5 +51,9 @@ const { data: lastReleases, pending, refresh, error } = await useLazyAsyncData(
   )
 )
 
-const lastRelease = computed(() => lastReleases.value[0])
+watchEffect(() => {
+  refresh()
+})
+
+const lastRelease = computed(() => lastReleases.value?.length ? lastReleases.value[0] : null)
 </script>
--- pages/releases/[release].vue
+++ pages/releases/[release].vue
@@ -1,24 +1,24 @@
 <template>
   <section v-if="post" class="max-w-[900px] my-8 mx-auto">
     <article class="flex flex-col">
-      <figure class="mx-16 lg:mx-0 flex items-center">
+      <figure class="mx-8 lg:mx-0 flex items-center">
         <img v-if="post.cover"
         class="w-full max-h-[600px] object-contain"
         draggable="false"
         :src="post.cover" />
       </figure>
       
-      <h1 class="text-center text-2xl lg:text-5xl text-primary-300 font-bold p-4 my-4">{{ post.title }}</h1>
+      <h1 class="mx-8 text-center text-2xl lg:text-5xl text-primary-300 font-bold my-4">{{ post.title }}</h1>
       <ContentRenderer :value="post">
         <ContentRendererMarkdown :value="post" />
         <template #empty>
         </template>
       </ContentRenderer>
       
-      <ul class="mx-16 my-8 flex justify-center flex-wrap gap-2">
+      <ul class="mx-8 xl:mx-16 my-8 flex justify-center flex-wrap gap-2">
         <li v-for="link in post.links"
         :key="link.id">
-          <a :href="link.url" class="block text-third-100 w-fit border-2 border-third-100 px-4 py-2 rounded-full hover:text-secondary-950 hover:bg-third-100 transition-all">
+          <a :href="link.url" class="block text-third-100 w-fit border-2 border-third-100 px-4 py-2 rounded-full hover:text-secondary-950 hover:bg-third-100 transition-all text-sm xl:text-base">
             {{ link.label }}
           </a>
         </li>
