f4ddd924fd4f8729d58946165f4ef6f1b9fcc4fe
dalien <daniel.cadeau13@gmail.com>
2024-07-23 23:10:30 +0000

    style: :lipstick: loading = simple spinner in the button

--- components/posts/posts.vue
+++ components/posts/posts.vue
@@ -17,14 +17,18 @@
       Posts not found
     </p>
     
-    <button v-if="!pending && hasMorePosts" @click="loadMorePosts" class="border-primary-300 border-[2px] text-primary-300 hover:bg-primary-300 transition-all hover:text-secondary-950 px-6 py-2 rounded-xl">Показать ещё</button>
+    <button v-if="pending || hasMorePosts"
+      @click="loadMorePosts"
+      :disabled="pending"
+      class="border-primary-300 border-[2px] text-primary-300 hover:bg-primary-300 transition-all hover:text-secondary-950 py-2 rounded-xl flex gap-4 pl-10 pr-6">
+      Показать ещё
+      <svg v-if="pending" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+      <span v-else></span>
+    </button>
   </div>
-  <ul v-if="pending" class="max-w-[900px] mx-8 lg:mx-auto grid lg:grid-cols-6 gap-8">
-    <li v-for="placeholder in placeholderClasses" :key="placeholder" class="w-full lg:col-span-3 animate-pulse bg-secondary-500 transition-all rounded-lg flex flex-col p-3">
-      <div class="w-full h-[410px] bg-secondary-800"></div>
-      <div class="mt-2 py-2 h-[44px] bg-secondary-800 text-third-100 rounded-md"></div>
-    </li>
-  </ul>
 </template>
 
 <script setup>
@@ -94,9 +98,5 @@ const loadMorePosts = async () => {
 }
 
 const hasMorePosts = computed(() => totalPosts.value > viewablePosts.value?.length + 1)
-
-const placeholderClasses = computed(() => {
-  return [...Array.from({ length: props.amount })]
-})
 
 </script>
